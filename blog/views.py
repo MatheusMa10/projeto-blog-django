a0376572634e626f47750a71c485e51fe749e9c4
Matheus Matos <matheeeusmatos@gmail.com>
2025-11-28 18:41:57 -0300

    .

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,333 @@
-from django.shortcuts import render, get_object_or_404
-from site_setup.models import SiteSetup
 
-# Create your views here.
+from typing import Any
+from django.core.paginator import Paginator
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect
+from blog.models import Post, Page
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.http import Http404, HttpRequest, HttpResponse
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    return render(
-        request,
-        'blog/pages/index.html',
-    )
+PER_PAGE = 9
+
+
+class PostListView(ListView):
+    # model  = Post
+    template_name = 'blog/pages/index.html'
+    context_object_name = 'posts'
+    # ordering = '-pk',
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published() #type:ignore
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({'page_title': 'Home - ',})
+
+        return context
+    
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+
+        page_title = 'Post de ' + user_full_name + ' - '
+
+        ctx.update({'page_title': page_title,})
+
+        return ctx
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # qs = qs.filter(self.kwargs.get('author_pk'))
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+    
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404
+        
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+        return super().get(request, *args, **kwargs)
+    
+    
+class CategoryListView(PostListView):
+    allow_empty = False
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug = self.kwargs.get('slug')
+        )
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        page_title = f'{self.kwargs['slug']} - categoria - '.title()
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+    
+class TagListView(PostListView):
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(tag__slug=self.kwargs['slug'])
+        return qs
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = f'{self.kwargs['slug']} - Tag - '.title()
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+
+
+class SearchListView(PostListView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._search_value = ''
+
+    def setup(self, request, *args, **kwargs):
+        self._search_value = request.GET.get('search', '').strip()
+        return super().setup(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        search_value = self._search_value
+        qs = super().get_queryset()
+        qs = qs.filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+
+        return qs
+
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        search_value = self._search_value
+        ctx.update({
+            'page_title': f'{search_value[:30]} - Search - ',
+            'search_value': search_value,
+        })
+
+        return ctx
+
+    def get(self, request, *args, **kwargs):
+        if self._search_value == '':
+            return redirect('blog:index')
+        return super().get(request, *args, **kwargs)
+
+
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+
+        page = self.get_object()
+
+        page_title = f'{page.title} - Página - ' #type:ignore
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(is_published=True)
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/pages/post.html'
+    slug_field = 'slug'
+    context_object_name = 'post'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(is_published=True)
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        ctx = super().get_context_data(**kwargs)
+
+        page = self.get_object()
+        page_title = f'{page.title} - Post - ' #type:ignore
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+
+
+# def index(request):
+#     posts =  Post.objects.get_published() # type: ignore
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': 'Home -'
+#         }
+#     )
+
+# def created_by(request, author_pk):
+#     user = User.objects.filter(pk=author_pk).first()
+
+#     if user is None:
+#         raise Http404()
+
+#     posts =  Post.objects.get_published().filter(created_by__pk=author_pk) # type: ignore
+#     user_full_name = user.username
+
+#     if user.first_name:
+#         user_full_name = f'{user.first_name} {user.last_name}'
+#     page_title = 'Posts de - ' + user_full_name + '-'
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+#         }
+#     )
+
+
+# def category(request, slug):
+#     posts =  Post.objects.get_published().filter(category__slug=slug) # type: ignore
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     if len(page_obj) == 0:
+#         raise Http404()
+
+#     page_title = f'{page_obj[0].category.name} - Categoria - '
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title
+#         }
+#     )
+
+# def tag(request, slug):
+#     posts =  Post.objects.get_published().filter(tag__slug=slug) # type: ignore
+    
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     if len(page_obj) == 0:
+#         raise Http404()
+
+#     page_title = f'{page_obj[0].tag.first().name} - tags - '
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': page_title,
+#         }
+#     )
+
+# def search(request):
+#     search_value = request.GET.get('search', '')
+#     posts =  (Post.objects.get_published() # type: ignore
+#               .filter(
+#                   Q(title__contains=search_value) |
+#                   Q(excerpt__contains=search_value) |
+#                   Q(content__contains=search_value)
+#               )
+#     )[:PER_PAGE]
+
+#     page_title = f'{search_value[:30]} - Search -'
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': posts,
+#             'search_value': search_value,
+#             'page_title': page_title,
+#         }
+#     )
+
+# def page(request, slug):
+
+#     page_obj = Page.objects.filter(slug=slug, is_published=True).first()
+
+#     if page_obj is None:
+#         raise Http404()
+    
+#     page_title = f'{page_obj.title} - Página - '
+
+#     return render(
+#         request,
+#         'blog/pages/page.html',
+#         {
+#             'page': page_obj,
+#             'page_title': page_title,
+#         }
+#     )
+
+
+# def post(request, slug):
+#     post_obj = Post.objects.get_published().filter(slug=slug).first() #type: ignore
+
+#     if post_obj is None:
+#         raise Http404()
+    
+#     page_title = f'{post_obj.title} - Post - '
+
+#     return render(
+#         request,
+#         'blog/pages/post.html',
+#         {
+#             'post': post_obj,
+#             'page_title': page_title,
+#         }
+#     )
--- project/settings.py
+++ project/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# DOTENV
+load_dotenv(BASE_DIR / 'dotenv_files' / '.env', override=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,8 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Meus apps
     'blog',
     'site_setup',
+
+    # summernote
+    'django_summernote',
+
+    # Axes
+    'axes',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +61,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # AxesMiddleware should be the last middleware in the MIDDLEWARE list.
+    # It only formats user lockout messages and renders Axes lockout responses
+    # on failed user authentication attempts from login views.
+    # If you do not want Axes to override the authentication response
+    # you can skip installing the middleware and use your own views.
+    'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -129,3 +148,43 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+SUMMERNOTE_CONFIG = {
+    'summernote': {
+        # Toolbar customization
+        # https://summernote.org/deep-dive/#custom-toolbar-popover
+        'toolbar': [
+            ['style', ['style', ]],
+            ['font', ['bold', 'italic', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph', 'hr', ]],
+            ['table', ['table']],
+            ['insert', ['link', 'picture']],
+            ['view', ['fullscreen', 'codeview', 'undo', 'redo']],
+        ],
+        'codemirror': {
+            'mode': 'htmlmixed',
+            'lineNumbers': 'true',
+            'lineWrapping': 'true',
+            'theme': 'dracula',
+        },
+    },
+    'css': (
+        '//cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/theme/dracula.min.css',
+    ),
+    'attachment_filesize_limit': 30 * 1024 * 1024,
+    # 'attachment_model': 'blog.PostAttachment',
+}
+
+AXES_ENABLED = True
+AXES_FAILURE_LIMIT = 3
+AXES_COOLOFF_TIME = 1  # 1 Hora
+AXES_RESET_ON_SUCCESS = True
